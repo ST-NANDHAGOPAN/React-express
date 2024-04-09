4bd91262889e6d0793bc66c24ae385ad3ebef137
Nandha <perachi.kailasam@ext.eyeball.club>
2024-04-09 12:15:31 +0530

    MC

--- client/src/_metronic/layout/components/header/header-menus/MenuInner.tsx
+++ client/src/_metronic/layout/components/header/header-menus/MenuInner.tsx
@@ -1,13 +1,9 @@
-import {MenuItem} from './MenuItem'
-import {MenuInnerWithSub} from './MenuInnerWithSub'
+// import {MenuItem} from './MenuItem'
+// import {MenuInnerWithSub} from './MenuInnerWithSub'
 
 export function MenuInner() {
   return (
     <>
-      <MenuInnerWithSub title='Apps' to='/apps' menuPlacement='bottom-start' menuTrigger='click'>
-        {/* PAGES */}
-        <MenuItem icon='abstract-35' to='/konva-management/konva' title='Konva JS' />
-      </MenuInnerWithSub>
  
     </>
   )
